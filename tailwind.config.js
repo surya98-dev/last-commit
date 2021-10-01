7324f8387f05878218ee5d72822b80df75a14c66
Faizhar Riskisyah <mohamad.faizhar@gmail.com>
2021-10-01 22:00:51 +0700

    refactor: add tailwind color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,36 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      btn: "#97B3FB",
+      dongker: "#202938",
+      main: "#F3F7FF",
+      navbar: "#E8ECF4",
+      orange: "#FFE0B2",
+      copy: "#332C2B",
+      transparent: "transparent",
+      current: "currentColor",
+      green: colors.green,
+      black: colors.black,
+      blue: colors.blue,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      blue: colors.blue,
+      red: colors.rose,
+      yellow: colors.amber,
+      green: colors.green,
+      lime: colors.lime,
+      violet: colors.violet,
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
